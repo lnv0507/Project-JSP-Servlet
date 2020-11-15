599505e9cc1094b0df15f68c62b77eaf3d2c3b42
lamnguyen-github <lamdilinh.nguyen@gmail.com>
2020-11-15 19:23:28 +0700

    Viết Báo Lỗi cho WEB tạo đăng ký

--- js/style.js
+++ js/style.js
@@ -16,3 +16,30 @@ function searchOut() {
         document.getElementById('mainmenu').style.display = 'block';
     }
 }
+
+function signIn() {
+    let name = document.getElementById('user');
+    let pass = document.getElementById('pass');
+    let passRe = document.getElementById('passRepeat');
+    let email = document.getElementById('email');
+    if (name.value == "") {
+        name.style.border = 'solid 1px red';
+    } else {
+        name.style.border = 'solid 1px green';
+    }
+    if (pass.value == "") {
+        pass.style.border = 'solid 1px red';
+    } else {
+        pass.style.border = 'solid 1px green';
+    }
+    if (passRe.value == "") {
+        passRe.style.border = 'solid 1px red';
+    } else {
+        passRe.style.border = 'solid 1px green';
+    }
+    if (email.value == "") {
+        email.style.border = 'solid 1px red';
+    } else {
+        email.style.border = 'solid 1px green';
+    }
+}
