c577bd0bbd5ce3dc332915061afb833974dd2824
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-01-18 14:01:56 +0700

    HELLO

--- src/main/java/com/controller/customer/link/LinkController.java
+++ src/main/java/com/controller/customer/link/LinkController.java
@@ -3,7 +3,6 @@ package com.controller.customer.link;
 public class LinkController {
     public static final String HOME = "index-2.jsp";
     public static final String PROJECT = "projects.jsp";
-    public static final String NEWFEED = "tintuc.jsp";
     public static final String ABOUT = "about.jsp";
     public static final String CONTACT = "contact.jsp";
     public static final String PROFILE = "profile.jsp";
