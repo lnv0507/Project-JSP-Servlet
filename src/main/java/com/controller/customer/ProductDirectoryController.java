f9269b39ec3e7db1ff76b67b2f2b446d033003d2
lamnguyen-github <18130121@st.hcmuaf.edu.vn>
2021-01-23 22:53:46 +0700

    Revert "12"
    This reverts commit cc02265d238c9154530f44808fb132e77c1acd8f.

--- src/main/java/com/controller/customer/ProductDirectoryController.java
+++ src/main/java/com/controller/customer/ProductDirectoryController.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 @WebServlet(name = "ProductDirectoryController", urlPatterns = "Directory")
 public class ProductDirectoryController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+
     }
 }
