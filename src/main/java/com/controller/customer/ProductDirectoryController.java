cf168122ddfaa443e8ad0f921b06896a46661679
moddbno <18130092@st.hcmuaf.edu.vn>
2021-01-24 08:19:08 +0700

    Update ProductDirectoryController.java

--- src/main/java/com/controller/customer/ProductDirectoryController.java
+++ src/main/java/com/controller/customer/ProductDirectoryController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ProductDirectoryController", urlPatterns = "Directory")
+@WebServlet(name = "ProductDirectoryController", urlPatterns = "/Directory")
 public class ProductDirectoryController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
